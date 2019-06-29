6a37a2c0408f49224303583d4b1afc36a7ea6ae8
ka1ot <52332162+ka1ot@users.noreply.github.com>
2019-06-30 02:36:58 +0300

    Update registration.php

--- authfolder/registration.php
+++ authfolder/registration.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Robolax</title>
+	<title>RobolaX</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="reg.css">
 </head>
 <body>
